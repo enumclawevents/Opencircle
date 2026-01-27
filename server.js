269f987a99c2d96ca9ccb33ecb77860919859751
Omedia LLC <omedia@Daniels-MacBook-Air-2.local>
2026-01-27 08:47:09 -0800

    Add Event Details and Good To Know fields

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use("/admin", adminRouter);
 // A simple test route so you can confirm the API is running
 app.get("/", (req, res) => {
   res.json({
