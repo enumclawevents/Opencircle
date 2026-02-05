05ddd8137253888db2efdd51a5b072f15e4329c6
Omedia LLC <omedia@Daniels-MacBook-Air-2.local>
2026-02-05 07:22:26 -0800

    Update admin recurrence UI logic

--- routes/admin.js
+++ routes/admin.js
@@ -854,7 +854,7 @@ return `
         position:absolute;
         top:0;
         bottom:-10px;
-        right:-16px;
+        right:-20px;
         width:1px;
         background: var(--sidebar-line);
       }
